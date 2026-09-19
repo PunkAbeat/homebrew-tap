5b30941f0d764e5359625bb8037024bb749e712e
David Lonjon <david.lonjon@gmail.com>
2026-09-19 15:19:34 +0900

    hostbeam 0.1.21

--- Casks/hostbeam.rb
+++ Casks/hostbeam.rb
@@ -1,6 +1,6 @@
 cask "hostbeam" do
-  version "0.1.20"
-  sha256 "8ac015214dc27b44da7a8b743793505bcc6a15e1e464ecc0a8abf7b351147569"
+  version "0.1.21"
+  sha256 "adc1367529bd502a44c8de01d23a6f126260cff39ac0867f29e8d89deb7b093e"
 
   url "https://download.hostbeam.app/v#{version}/Hostbeam_#{version}.dmg"
   name "Hostbeam"
